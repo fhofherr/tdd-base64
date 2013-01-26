76e6c26dd2d6103191e567cac12d3e02866647c2
Ferdinand Hofherr <mail@ferdinandhofherr.de>
2013-01-26 15:52:14 +0100

    Make first baby-step less silly.

--- src/main/java/de/ferdinandhofherr/tddbase64/OctetRegrouper.java
+++ src/main/java/de/ferdinandhofherr/tddbase64/OctetRegrouper.java
@@ -1,11 +1,27 @@
 package de.ferdinandhofherr.tddbase64;
 
-
-
 public class OctetRegrouper {
 
+    private static final int FIRST_6_BIT  = 0xFC0000;
+    private static final int SECOND_6_BIT = 0x03F000;
+    private static final int THIRD_6_BIT  = 0x000FC0;
+    private static final int FOURTH_6_BIT = 0x00003F;
+
     public static byte[] regroup(byte[] inputOctets) {
-        return new byte[] {(byte) 0x3F, (byte) 0x3F, (byte) 0x3F, (byte) 0x3F};
+        byte[] sixBitGroups = new byte[4];
+
+        int joinedOctets = 0;
+        for (byte b : inputOctets) {
+            joinedOctets <<= 8;
+            joinedOctets |= (b & 0xFF);
+        }
+
+        sixBitGroups[0] = (byte) (joinedOctets & FIRST_6_BIT >> 18);
+        sixBitGroups[1] = (byte) (joinedOctets & SECOND_6_BIT >> 12);
+        sixBitGroups[2] = (byte) (joinedOctets & THIRD_6_BIT >> 6);
+        sixBitGroups[3] = (byte) (joinedOctets & FOURTH_6_BIT);
+
+        return sixBitGroups;
     }
 
 }
